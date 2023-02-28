9a03bed8cb7d7c9b03516d5b0bc0db1196e93123
João Carlos <jcsra2002@gmail.com>
2023-02-28 13:51:24 -0300

    Projeto concluído

--- script.js
+++ script.js
@@ -1,16 +1,41 @@
-const borderRange = document.getElementById('border-range');
-const rangeValue = document.getElementById('range-value');
-const label = document.querySelector('label')
+const borderRange = document.querySelectorAll('#switch-4-container .border-range');
+const border_one_Range = document.getElementById('border-1-range');
+const rangeInput = document.getElementById('range-1-value');
+const label = document.querySelector('label[for="border-1-range"]');
+const valueCss = document.getElementById('value-css');
+
 window.addEventListener('load', () => {
-    borderRange.addEventListener('change', () => {
-        label.textContent = borderRange.value + ' %'
-        rangeValue.value = borderRange.value
-        document.getElementById('border').style.borderRadius = borderRange.value + '%'
+    border_one_Range.addEventListener('change', () => {
+        label.textContent = border_one_Range.value + ' %'
+        rangeInput.value = border_one_Range.value
+        document.getElementById('border').style.borderRadius = border_one_Range.value + '%'
+        valueCss.textContent = 'border-radius: ' + document.getElementById('border').style.borderRadius + ';'
     })
-    rangeValue.addEventListener('change', () => {
-        borderRange.value = rangeValue.value
-        rangeValue.value = borderRange.value
-        label.textContent = borderRange.value + ' %'
-        document.getElementById('border').style.borderRadius = borderRange.value + '%'
+    rangeInput.addEventListener('change', () => {
+        border_one_Range.value = rangeInput.value
+        rangeInput.value = border_one_Range.value
+        label.textContent = border_one_Range.value + ' %'
+        document.getElementById('border').style.borderRadius = border_one_Range.value + '%'
+    })
+    borderRange.forEach((range) => {
+        range.addEventListener('change', () => {
+            const border = document.getElementById('border');
+            if (range.id === 'border-range1') {
+                document.querySelector('#switch-4-container label[for="' + range.id + '"]').textContent = range.value + ' %'
+                document.getElementById('border').style.borderTopLeftRadius = range.value + '%'
+            } else if (range.id === 'border-range2') {
+                document.querySelector('#switch-4-container label[for="' + range.id + '"]').textContent = range.value + ' %'
+                document.getElementById('border').style.borderTopRightRadius = range.value + '%'
+            } else if (range.id === 'border-range3') {
+                document.querySelector('#switch-4-container label[for="' + range.id + '"]').textContent = range.value + ' %'
+                document.getElementById('border').style.borderBottomLeftRadius = range.value + '%'
+            } else {
+                document.querySelector('#switch-4-container label[for="' + range.id + '"]').textContent = range.value + ' %'
+                document.getElementById('border').style.borderBottomRightRadius = range.value + '%'
+            } valueCss.textContent = 'border-radius: ' + border.style.borderTopLeftRadius + ' ' + border.style.borderTopRightRadius + ' ' + border.style.borderBottomLeftRadius + ' ' + border.style.borderBottomRightRadius    
+        })
+    })
+    document.getElementById('copy-paste').addEventListener('click', () => {
+        navigator.clipboard.writeText(valueCss.textContent)
     })
 })
